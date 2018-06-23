f83ccf83a5c52ab5b285173d55f2824e7d608cca
Vu Cao <cdhvu@yahoo.com>
2018-06-23 11:58:55 -0700

    Completed the App

--- 1. Javascript Drumkit/index.js
+++ 1. Javascript Drumkit/index.js
@@ -1,2 +1,20 @@
-const buttons = document.querySelectorAll('.button');
-buttons.forEach(button => button.addEventListener('click', ()=>button.classList.add('clicked')));
+window.addEventListener('keydown', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const button = document.querySelector(`.button[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 1;
+    audio.play();
+    button.classList.add('clicked');
+
+});
+
+const removeTransition = (e) => {
+    if (e.propertyName !== 'transform') {
+       return;
+    }
+    e.target.classList.remove('clicked');
+}
+
+const keys = document.querySelectorAll('.button');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
